83654bee27a9c8533cf6b50ef2f6ac522f839989
Shivangi Jha <shivangi@Shivangis-MacBook-Air.local>
2023-05-13 23:25:29 +0530

    first commit

---
+++ shmtool/src/main/java/io/commerce/shmtool/ShmtoolApplication.java
@@ -0,0 +1,13 @@
+package io.commerce.shmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShmtoolApplication.class, args);
+	}
+
+}
---
+++ shmtool/src/test/java/io/commerce/shmtool/ShmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.commerce.shmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
